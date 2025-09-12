8028834f32091d4851ec3d6fafb43465bb6c8acb
Masuk Khan <masukkhanemail@gmail.com>
2025-09-12 09:54:38 +0530

    first api call, userRegister

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin:"http://localhost:5174",
+  origin:["http://localhost:5173","http://localhost:5174"],
   credentials:true,
 }));
 
