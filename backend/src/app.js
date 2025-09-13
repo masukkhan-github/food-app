22a9dad9cfd4d657c6b4df3d66563e13abb63a7d
Masuk Khan <masukkhanemail@gmail.com>
2025-09-13 16:21:38 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin:["http://localhost:5173","http://localhost:5174"],
+  origin:["https://food-app-frontend-6tzq.onrender.com","http://localhost:5174"],
   credentials:true,
 }));
 
