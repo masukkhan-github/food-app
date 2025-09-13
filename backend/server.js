1e48d80ea296b048388c9988c84a1e0ba38cf78b
Masuk Khan <masukkhanemail@gmail.com>
2025-09-13 16:01:41 +0530

    env add

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import app from "./src/app.js";
 import { connectDB } from "./src/db/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
  connectDB();
 
-app.listen(3000,()=>{
-    console.log(`app is running on port ${3000}` );
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`app is running on port ${process.env.PORT || 3000}` );
 });
 
